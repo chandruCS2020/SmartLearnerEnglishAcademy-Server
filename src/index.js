5566abf7b18a4e8a19edc6c51c9503d8025410f3
Chandru <chandru6501@gmail.com>
2021-12-19 17:48:27 +0530

    test

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser(process.env.JWTCOOKIESECRET))
 app.use(express.urlencoded({extended:false}))
 app.use((req,res,next)=>{
-    res.set("Access-Control-Allow-Origin","http://localhost:3001/");
+    res.set("Access-Control-Allow-Origin","http://localhost:3001");
     res.set("Access-Control-Allow-Credentials","true");
     res.set("Access-Control-Allow-Headers","Content-type, Set-Cookie");
     next();
