fed0e50b48d7003c6076a97895a66d52133a694d
Chandru <chandru6501@gmail.com>
2022-02-06 11:52:31 +0530

    o

--- src/routes/contactRoutes.js
+++ src/routes/contactRoutes.js
@@ -8,7 +8,7 @@ app.post("/Contact",async (req,res)=>{
     try{
         let contact=new Contact(req.body);
         await contact.save();
-        await sendEmail("chandru6501@gmail.com","Contact",JSON.stringify(contact));
+        await sendEmail(req.email,req.subject,JSON.stringify(contact));
         res.send("Our Emplyoee will Contact you in 4-5 business days")
     }catch(err){
         res.status(400).send(err.message);
