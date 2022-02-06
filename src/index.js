3d03eadecab7cbefae77a8bc2e536573650cc19c
Chandru <chandru6501@gmail.com>
2022-02-06 11:07:41 +0530

    o

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser(process.env.JWTCOOKIESECRET))
 app.use(express.urlencoded({extended:false}))
 app.use((req,res,next)=>{
-    res.set("Access-Control-Allow-Origin","https://eruditesea.com");
+    res.set("Access-Control-Allow-Origin","http://localhost:3000");
     res.set("Access-Control-Allow-Credentials","true");
     res.set("Access-Control-Allow-Headers","Content-type, Set-Cookie");
     next();
