848c9794f5b62e1dbc9dacb9477ea4819b5e0955
Chandru <chandru6501@gmail.com>
2022-02-06 12:18:17 +0530

    o

--- src/routes/contactRoutes.js
+++ src/routes/contactRoutes.js
@@ -8,7 +8,25 @@ app.post("/Contact",async (req,res)=>{
     try{
         let contact=new Contact(req.body);
         await contact.save();
-        await getEmail(req.body.email,req.body.subject,JSON.stringify(contact));
+        await getEmail(req.body.email,req.body.subject,`<div class="container" style="background-color: #ddd;text-align: center;max-width: 600px;margin: auto;">
+        <div class="main" style="background-color: #003399;padding: 2%;color: #fff;">
+            <h4 style="font-weight: 400;font-size: 2rem;">Contact</h4>
+        </div>
+        <div class="body" style="padding: 10%;font-weight: 400;color: rgba(0, 0, 0, 0.747);">
+            <div class="name" style="font-size: 1.2rem; padding: 10px;">
+                <span>Name</span> <span>:</span> <span>${req.body.name}</span>
+            </div>
+            <div class="name"  style="font-size: 1.2rem; padding: 10px;">
+                <span>Mobile Number</span> <span>:</span> <span>${req.body.email}</span>
+            </div>
+            <div class="name"  style="font-size: 1.2rem; padding: 10px;">
+                <span>Email</span> <span>:</span> <span>${req.body.mobile}</span>
+            </div>
+            <div class="name"  style="font-size: 1.2rem; padding: 10px;">
+                <span>Message</span> <span>:</span> <span>${req.body.message}</span>
+            </div>
+        </div>
+    </div>`);
         res.send("Our Emplyoee will Contact you in 4-5 business days")
     }catch(err){
         res.status(400).send(err.message);
