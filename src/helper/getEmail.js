b4155d4a25275fa4de2ca37b9687af004673eb66
Chandru <chandru6501@gmail.com>
2022-05-23 19:17:45 +0530

    email added

--- src/helper/getEmail.js
+++ src/helper/getEmail.js
@@ -12,7 +12,7 @@ async function getEmail(to,subject,body){
     });
     
     transporter.sendMail({
-        to:process.env.EMAIL, 
+        to:process.env.EMAIL_USER, 
         subject,
         html: body
     })
